da356b687d066fb9d807d813794ac919f0b9e339
Luiz Pereira de Souza Filho <lpsouza@gmail.com>
2022-10-17 01:39:42 -0300

    Added localhost url on initial message

--- server.ts
+++ server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, (): void => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
